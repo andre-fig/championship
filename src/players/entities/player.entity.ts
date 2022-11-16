58f8e8373bccefb025be14cf3738efc2be858fad
André Figueiredo <afigueiredo@tasken.com>
2022-11-16 20:08:39 -0300

    feat: Adicionando Entidade Player

--- src/players/entities/player.entity.ts
+++ src/players/entities/player.entity.ts
@@ -1,1 +1,24 @@
-export class Player {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Player {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  cpf: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'int' })
+  unity: number;
+}
+
+// adicionar campeonato
