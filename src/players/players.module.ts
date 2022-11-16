0cf76683e990c708e2493aaa0105d582536c46fb
André Figueiredo <afigueiredo@tasken.com>
2022-11-16 20:54:05 -0300

    feat: Adicionando Entidade Player

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
+import { Player } from './entities/player.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Player])],
   controllers: [PlayersController],
-  providers: [PlayersService]
+  providers: [PlayersService],
+  exports: [PlayersService],
 })
 export class PlayersModule {}
